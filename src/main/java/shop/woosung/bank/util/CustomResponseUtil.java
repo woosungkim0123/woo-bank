2efc408f28a79fab6e95f6219b7e3efc8bee4bed
Woosung_Kim <sibu2005@naver.com>
2023-06-15 21:35:54 +0900

    refactor : 간단한 주석 및 자리 맞춤 (#7)

--- src/main/java/shop/woosung/bank/util/CustomResponseUtil.java
+++ src/main/java/shop/woosung/bank/util/CustomResponseUtil.java
@@ -12,10 +12,6 @@ public class CustomResponseUtil {
 
     private static final Logger log = LoggerFactory.getLogger(CustomResponseUtil.class);
 
-    /**
-     * 401은 인증이 안된 것
-     * 403은 권한이 없는 것
-     */
     public static void unAuthentication(HttpServletResponse response, String msg) {
         try {
             ObjectMapper om = new ObjectMapper();
