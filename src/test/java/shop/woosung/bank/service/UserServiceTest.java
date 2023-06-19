da065006e656005452e692bf7eb7a6b47bbacee4
Woosung_Kim <sibu2005@naver.com>
2023-06-19 21:38:25 +0900

    refactor : 테스트 리팩토링 및 더미 데이터 생성 분리 (#7)
    - 테스트 리팩토링
    - 더미 유저 데이터 분리

--- src/test/java/shop/woosung/bank/service/UserServiceTest.java
+++ src/test/java/shop/woosung/bank/service/UserServiceTest.java
@@ -1,86 +1,56 @@
 package shop.woosung.bank.service;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import shop.woosung.bank.domain.user.User;
 import shop.woosung.bank.domain.user.UserEnum;
 import shop.woosung.bank.domain.user.repository.UserRepository;
-import shop.woosung.bank.service.UserService.SignUpResDto;
+import shop.woosung.bank.dto.user.UserReqDto;
+import shop.woosung.bank.dto.user.UserResDto;
+import shop.woosung.bank.util.dummy.DummyUserObject;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class) // 모키토 환경에서 테스트 진행
-class UserServiceTest {
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest extends DummyUserObject {
 
-    // 가짜환경을 만들어서 넣음
     @InjectMocks
     private UserService userService;
 
     @Mock
     private UserRepository userRepository;
 
-    // 진짜를 가짜에 집어넣는 방법(가짜가 아니라 진짜를 띄우는 방법) :: Mock과 차이
-    // spy를 사용하면 spring ioc에 있는 걸 꺼내서 InjectMock에 집어넣음
-    // userService에 userRepository는 가짜지만 passwordEncoder는 진짜임
     @Spy
     private BCryptPasswordEncoder passwordEncoder;
 
     @Test
     void 회원가입_test() throws Exception {
         // given
-        UserService.SignUpReqDto signUpReqDto =  new UserService.SignUpReqDto();
+        UserReqDto.SignUpReqDto signUpReqDto =  new UserReqDto.SignUpReqDto();
         signUpReqDto.setUsername("user1");
         signUpReqDto.setPassword("1234");
-        signUpReqDto.setEmail("sibu2005@Naver.com");
-        signUpReqDto.setFullname("쌀");
-
-        // stub1 : 가정법, 가설
-        // 니가 이걸 실행하면 이게 리턴될거야
-        // 매개변수에 뭐라도 들어가면
-        //when(userRepository.findByUsername(any())).thenReturn(Optional.empty());
-        when(userRepository.findByUsername(any())).thenReturn(Optional.of(new User()));
-
+        signUpReqDto.setEmail("test@naver.com");
+        signUpReqDto.setFullname("테스터");
+        // stub1
+        when(userRepository.findByUsername(any())).thenReturn(Optional.empty());
         // stub2
-        User woo = User.builder()
-                .id(1L)
-                .username("user1")
-                .password("1234")
-                .email("sibu2005@Naver.com")
-                .fullname("쌀")
-                .role(UserEnum.CUSTOMER)
-                .createdAt(LocalDateTime.now())
-                .updatedAt(LocalDateTime.now())
-                .build();
-
-        when(userRepository.save(any())).thenReturn(woo);
+        User mockUser = newMockUser(1L, "user1", "1234", "test@naver.com", "테스터", UserEnum.CUSTOMER);
+        when(userRepository.save(any())).thenReturn(mockUser);
 
         // when
+        UserResDto.SignUpResDto signUpResDto = userService.signUp(signUpReqDto);
 
-        SignUpResDto signUpResDto = userService.signUp(signUpReqDto);
-        // 그대로하면 에러뜸 null pointer -> findByUsername에서 터짐
-        // Mock = 가짜 환경이라서 스프링 관련 빈들이 하나도 없는 상태
-        // 가짜환경에다가 서비스할때는 필요한걸 등록해줘야함
-        // private UserRepository userRepository;
-        // 근데 우리가 서비스를 테스트하는거지 userRepository를 테스트하는게 아님
-        // 가짜로 메모리에 띄워서 InjectMocks 어노테이션 UserService에 주입을 해줌
-        // 메서드가 없어서 nullPointer가 뜸 stub이라는 처리를 해줘야함
-        // findByUsername과 save 둘다 stub 처리를 하고
-        // bcrpt에 encoder에 대한 처리를 해줘야함
-        System.out.println(signUpResDto);
-
-        Assertions.assertThat(signUpResDto.getId()).isEqualTo(1L);
-        Assertions.assertThat(signUpResDto.getUsername()).isEqualTo("user1");
+        // then
+        assertThat(signUpResDto.getId()).isEqualTo(1L);
+        assertThat(signUpResDto.getUsername()).isEqualTo("user1");
     }
 }
---
+++ src/test/java/shop/woosung/bank/util/dummy/DummyUserObject.java
@@ -0,0 +1,35 @@
+package shop.woosung.bank.util.dummy;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import shop.woosung.bank.domain.user.User;
+import shop.woosung.bank.domain.user.UserEnum;
+
+import java.time.LocalDateTime;
+
+public class DummyUserObject {
+
+    protected User newUser(String username, String password, String email, String fullname, UserEnum role) {
+        String encodingPassword = new BCryptPasswordEncoder().encode(password);
+        return User.builder()
+                .username(username)
+                .password(encodingPassword)
+                .email(email)
+                .fullname(fullname)
+                .role(role)
+                .build();
+    }
+
+    protected User newMockUser(Long id, String username, String password, String email, String fullname, UserEnum role) {
+        String encodingPassword = new BCryptPasswordEncoder().encode(password);
+        return User.builder()
+                .id(id)
+                .username(username)
+                .password(encodingPassword)
+                .email(email)
+                .fullname(fullname)
+                .role(role)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+    }
+}
