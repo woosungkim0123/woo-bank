47abf4464959997a6bec2713391c1c50c23f97a8
Woosung_Kim <sibu2005@naver.com>
2023-06-25 23:32:51 +0900

    feat : 유저 로그인 요청과 응답을 위한 dto (#9)
    - 날짜 변형을 유틸 클래스로 분리

---
+++ src/main/java/shop/woosung/bank/util/CustomDateUtil.java
@@ -0,0 +1,12 @@
+package shop.woosung.bank.util;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class CustomDateUtil {
+
+    public static String toStringFormat(LocalDateTime localDateTime) {
+        return localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+}
