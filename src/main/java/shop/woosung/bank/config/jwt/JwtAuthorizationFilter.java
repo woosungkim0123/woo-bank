2483ca72de54eee75264ad0cdea5c90b80446445
Woosung_Kim <sibu2005@naver.com>
2023-07-11 21:19:25 +0900

    bugfix : 토큰 만료, 검증, 서버에러 예외처리 추가 (#9)

--- src/main/java/shop/woosung/bank/config/jwt/JwtAuthorizationFilter.java
+++ src/main/java/shop/woosung/bank/config/jwt/JwtAuthorizationFilter.java
@@ -1,11 +1,18 @@
 package shop.woosung.bank.config.jwt;
 
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import shop.woosung.bank.config.auth.LoginUser;
+import shop.woosung.bank.util.CustomResponseUtil;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -16,31 +23,38 @@ import java.io.IOException;
 /*
     모든 주소에 토큰을 검증
  */
+@Slf4j
 public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
-
 
     public JwtAuthorizationFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
     }
 
-
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-        if(isHeaderVerify(request)) {
-            String token = request.getHeader(JwtVO.HEADER).replace(JwtVO.TOKEN_PREFIX, "");
-            LoginUser loginUser = JwtProcess.verify(token);
-            Authentication authenticationToken = new UsernamePasswordAuthenticationToken(loginUser, null, loginUser.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) {
+        try {
+            if(isHeaderVerify(request)) {
+                String token = request.getHeader(JwtVO.HEADER).replace(JwtVO.TOKEN_PREFIX, "");
+                LoginUser loginUser = JwtProcess.verify(token);
+                Authentication authenticationToken = new UsernamePasswordAuthenticationToken(loginUser, null, loginUser.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+            chain.doFilter(request, response);
+        } catch (TokenExpiredException e) {
+            log.error("token expired = {}", e);
+            CustomResponseUtil.fail(response, "토큰이 만료되었습니다.", HttpStatus.UNAUTHORIZED);
+        } catch (JWTVerificationException e) {
+            log.error("token fail = {}", e);
+            CustomResponseUtil.fail(response, "토큰 검증에 실패했습니다.", HttpStatus.UNAUTHORIZED);
+        } catch (IOException | ServletException e) {
+            log.error("IOException | ServletException = {}", e);
+            CustomResponseUtil.fail(response, "서버 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        chain.doFilter(request, response);
     }
 
     private boolean isHeaderVerify(HttpServletRequest request) {
         String header = request.getHeader(JwtVO.HEADER);
-        if (header == null || !header.startsWith(JwtVO.TOKEN_PREFIX)) {
-            return false;
-        }
-        return true;
+        return header != null;
     }
 
 }
