ad6e249400bb50f3e781f498b7bd2d18b45d5b35
Woosung_Kim <sibu2005@naver.com>
2023-07-12 23:23:08 +0900

    feat : 가장 최신의 계좌번호를 찾아서 하나 반환 (#13)
    - 계좌번호에서 + 1을 해서 다음 계좌번호를 만들기 위해

--- src/main/java/shop/woosung/bank/domain/account/repository/AccountRepository.java
+++ src/main/java/shop/woosung/bank/domain/account/repository/AccountRepository.java
@@ -3,5 +3,9 @@ package shop.woosung.bank.domain.account.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shop.woosung.bank.domain.account.Account;
 
+import java.util.Optional;
+import java.util.stream.Stream;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findFirstByOrderByNumberDesc();
 }
