6c0e4451b08a89f28000215bb0e2d296160f1019
Woosung_Kim <sibu2005@naver.com>
2023-08-14 23:37:27 +0900

    refactor : 시큐리티 수정, 에러 구조 변경 필요 (#30)

--- src/main/java/shop/woosung/bank/config/SecurityConfig.java
+++ src/main/java/shop/woosung/bank/config/SecurityConfig.java
@@ -2,84 +2,93 @@ package shop.woosung.bank.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import shop.woosung.bank.config.jwt.JwtAuthenticationFilter;
-import shop.woosung.bank.config.jwt.JwtAuthorizationFilter;
-import shop.woosung.bank.config.jwt.JwtHolder;
+import shop.woosung.bank.config.filter.JwtAuthenticationFilter;
+import shop.woosung.bank.config.filter.JwtAuthorizationFilter;
+import shop.woosung.bank.config.jwt.JwtTokenProvider;
 import shop.woosung.bank.user.domain.UserRole;
 
-import shop.woosung.bank.util.CustomResponseUtil;
+import java.util.Arrays;
 
 @Slf4j
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
 
-    private final JwtHolder jwtHolder;
+    private final Environment environment;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        log.debug("디버그 : BCryptPasswordEncoder 빈 등록됨");
-        return new BCryptPasswordEncoder();
-    }
-
-    // TODO2 : 권한 에러처리 필요
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.headers().frameOptions().disable();
-        http.csrf().disable();
-        http.cors().configurationSource(configurationSource());
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.formLogin().disable();
-        http.httpBasic().disable();
+        configureDevSettings(http);
 
-        http.apply(new CustomSecurityFilterManager());
-
-
-        http.exceptionHandling()
-                .authenticationEntryPoint((req, res, e) -> CustomResponseUtil.fail(res, "로그인을 진행 해주세요", HttpStatus.UNAUTHORIZED))
-                .accessDeniedHandler((req, res, e) -> CustomResponseUtil.fail(res, "권한이 없습니다", HttpStatus.FORBIDDEN));
-
-        http.authorizeHttpRequests()
-                .antMatchers("/api/s/**").authenticated()
-                .antMatchers("/api/admin/**").hasRole("" + UserRole.ADMIN)
-                .anyRequest().permitAll();
-
-        return http.build();
+        return http
+                    .cors(config -> config.configurationSource(configureCors()))
+                    .sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    .formLogin(AbstractHttpConfigurer::disable)
+                    .httpBasic(AbstractHttpConfigurer::disable)
+                    .authorizeRequests(config -> config
+                            .antMatchers("/api/s/**").authenticated()
+                            .antMatchers("/api/admin/**").hasRole(UserRole.ADMIN.name())
+                            .anyRequest().permitAll())
+                // todo ->handler로못잡음->시큐리티에서잡자
+//                    .exceptionHandling(config -> {
+//
+//                        config
+//                                .authenticationEntryPoint((req, res, e) -> {
+//                                    throw new AException("11");
+//                                });
+//                            }
+//
+//                    )
+                    .apply(new CustomSecurityFilterManager()).and()
+                    .build();
     }
 
-    public CorsConfigurationSource configurationSource() {
+    private class CustomSecurityFilterManager extends AbstractHttpConfigurer<CustomSecurityFilterManager, HttpSecurity> {
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            AuthenticationManager authenticationManager = getBuilder().getSharedObject(AuthenticationManager.class);
+            getBuilder().addFilter(new JwtAuthenticationFilter(jwtTokenProvider, authenticationManager));
+            getBuilder().addFilter(new JwtAuthorizationFilter(jwtTokenProvider, authenticationManager));
+            super.configure(http);
+        }
+    }
+
+    private CorsConfigurationSource configureCors() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
-        configuration.addAllowedOriginPattern("*");
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
-        // 안해도 되는 것 같은데?
-        // configuration.addExposedHeader("Authorization");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 
-    public class CustomSecurityFilterManager extends AbstractHttpConfigurer<CustomSecurityFilterManager, HttpSecurity> {
 
-        @Override
-        public void configure(HttpSecurity http) throws Exception {
-            AuthenticationManager authenticationManager = getBuilder().getSharedObject(AuthenticationManager.class);
-            getBuilder().addFilter(new JwtAuthenticationFilter(jwtHolder, authenticationManager));
-            getBuilder().addFilter(new JwtAuthorizationFilter(jwtHolder, authenticationManager));
-            super.configure(http);
+    private void configureDevSettings(HttpSecurity http) throws Exception {
+        if (isDevProfileActive()) {
+            http
+                .headers(config -> config.frameOptions().disable())
+                .csrf(AbstractHttpConfigurer::disable);
         }
+    }
+
+    private boolean isDevProfileActive() {
+        return Arrays.asList(environment.getActiveProfiles()).contains("dev");
     }
 }
