e8035e5efea3d04a8855cf183117300360ee27a1
Woosung_Kim <sibu2005@naver.com>
2023-09-23 22:51:08 +0900

    refactor : 계좌 출금 기능을 도메인이 실행하도록 변경 (#38)

---
+++ src/main/java/shop/woosung/bank/transaction/domain/WithdrawTransactionCreate.java
@@ -0,0 +1,25 @@
+package shop.woosung.bank.transaction.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import shop.woosung.bank.account.domain.Account;
+
+@Getter
+public class WithdrawTransactionCreate {
+    private final Account withdrawAccount;
+    private final Long withdrawAccountBalance;
+    private final Long amount;
+    private final TransactionType type;
+    private final String sender;
+    private final String receiver;
+
+    @Builder
+    public WithdrawTransactionCreate(Account withdrawAccount, Long withdrawAccountBalance, Long amount, TransactionType type, String sender, String receiver, String tel) {
+        this.withdrawAccount = withdrawAccount;
+        this.withdrawAccountBalance = withdrawAccountBalance;
+        this.amount = amount;
+        this.type = type;
+        this.sender = sender;
+        this.receiver = receiver;
+    }
+}
