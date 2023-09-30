8af354e3e53e306566ba02db7355971027b6d9d0
Woosung_Kim <sibu2005@naver.com>
2023-09-30 22:59:44 +0900

    refactor : 계좌 서비스 테스트 코드 - mockito 사용으로 변경 (#40)

--- src/test/java/shop/woosung/bank/account/service/AccountServiceImplTest.java
+++ src/test/java/shop/woosung/bank/account/service/AccountServiceImplTest.java
@@ -1,96 +1,86 @@
 package shop.woosung.bank.account.service;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import shop.woosung.bank.account.controller.port.AccountService;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import shop.woosung.bank.account.controller.port.AccountLockService;
 import shop.woosung.bank.account.domain.Account;
-import shop.woosung.bank.account.domain.AccountSequence;
 import shop.woosung.bank.account.domain.AccountType;
 import shop.woosung.bank.account.domain.AccountTypeNumber;
+import shop.woosung.bank.account.handler.exception.NotAccountOwnerException;
 import shop.woosung.bank.account.handler.exception.NotFoundAccountFullNumberException;
-import shop.woosung.bank.account.handler.exception.NotFoundAccountSequenceException;
 import shop.woosung.bank.account.handler.exception.NotFoundAccountTypeNumberException;
 import shop.woosung.bank.account.service.dto.*;
 import shop.woosung.bank.account.service.port.AccountRepository;
-import shop.woosung.bank.mock.repository.*;
-import shop.woosung.bank.mock.util.FakePasswordEncoder;
+import shop.woosung.bank.account.service.port.AccountTypeNumberRepository;
+import shop.woosung.bank.transaction.domain.Transaction;
 import shop.woosung.bank.transaction.domain.TransactionType;
+import shop.woosung.bank.transaction.service.port.TransactionRepository;
 import shop.woosung.bank.user.domain.User;
-import shop.woosung.bank.user.service.port.UserRepository;
 
-
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class AccountServiceImplTest {
-
-    private AccountService accountService;
-    private UserRepository userRepository;
+    @InjectMocks
+    private AccountServiceImpl accountService;
+    @Mock
     private AccountRepository accountRepository;
-    private FakeAccountSequenceRepository accountSequenceRepository;
-    private FakeAccountTypeNumberRepository accountTypeNumberRepository;
-    private FakePasswordEncoder passwordEncoder;
+    @Mock
+    private AccountLockService accountLockService;
+    @Mock
+    private AccountTypeNumberRepository accountTypeNumberRepository;
+    @Mock
+    private TransactionRepository transactionRepository;
 
-    private static final Long incrementByNumber = 1L;
-    private static final Long initNormalTypeNumber = 232L;
-    private static final Long initSavingTypeNumber = 787L;
-
-    @BeforeEach
-    void init() {
-        Long initSequenceNumber = 11111111111L;
-
-        userRepository = new FakeUserRepository();
-        accountRepository = new FakeAccountRepository();
-        accountSequenceRepository = new FakeAccountSequenceRepository();
-        accountTypeNumberRepository = new FakeAccountTypeNumberRepository();
-        passwordEncoder = new FakePasswordEncoder("aaaa_bbbb_cccc_dddd");
-
-        this.accountService = AccountServiceImpl.builder()
-                .passwordEncoder(passwordEncoder)
-                .accountRepository(accountRepository)
-                .accountTypeNumberRepository(accountTypeNumberRepository)
-                .accountLockService(new AccountLockServiceImpl(accountRepository, passwordEncoder, null))
-                .transactionRepository(new FakeTransactionRepository())
-                .build();
-
-
-        accountSequenceRepository.save(AccountSequence.builder().sequenceName(AccountType.NORMAL).nextValue(initSequenceNumber).incrementBy(incrementByNumber).build());
-        accountSequenceRepository.save(AccountSequence.builder().sequenceName(AccountType.SAVING).nextValue(initSequenceNumber).incrementBy(incrementByNumber).build());
-        accountTypeNumberRepository.save(AccountTypeNumber.builder().accountType(AccountType.NORMAL).number(initNormalTypeNumber).build());
-        accountTypeNumberRepository.save(AccountTypeNumber.builder().accountType(AccountType.SAVING).number(initSavingTypeNumber).build());
-    }
-
-    @DisplayName("자신의 모든 계좌 목록을 가져온다")
+    @DisplayName("자신의 모든 계좌 목록을 가져온다.")
     @Test
-    public void get_my_all_account() {
+    void get_my_all_accounts() {
         // given
-        User user1 = userRepository.save(User.builder().email("test1@tset.com").name("test1").build());
-        User user2 = userRepository.save(User.builder().email("test2@tset.com").name("test2").build());
-        accountRepository.save(Account.builder().number(1111111111L).balance(1000L).user(user1).build());
-        accountRepository.save(Account.builder().number(1111111112L).balance(2000L).user(user1).build());
-        accountRepository.save(Account.builder().number(1111111113L).balance(3000L).user(user2).build());
+        User user = User.builder().id(1L).name("test").build();
+        when(accountRepository.findByUserId(anyLong())).thenReturn(
+                List.of(
+                        Account.builder().id(1L).fullNumber(23211111111L).type(AccountType.NORMAL).balance(1000L).user(user).build(),
+                        Account.builder().id(2L).fullNumber(23211111112L).type(AccountType.SAVING).balance(2000L).user(user).build()
+                ));
 
         // when
-        AccountListResponseDto result = accountService.getAccountList(user1);
+        AccountListResponseDto result = accountService.getAccountList(user);
 
         // then
+        assertThat(result.getUsername()).isEqualTo("test");
         assertThat(result.getAccounts().size()).isEqualTo(2);
-        assertThat(result.getAccounts().get(0).getNumber()).isEqualTo(1111111111L);
+        assertThat(result.getAccounts().get(0).getId()).isEqualTo(1L);
+        assertThat(result.getAccounts().get(0).getFullNumber()).isEqualTo(23211111111L);
+        assertThat(result.getAccounts().get(0).getType()).isEqualTo(AccountType.NORMAL);
         assertThat(result.getAccounts().get(0).getBalance()).isEqualTo(1000L);
-        assertThat(result.getAccounts().get(1).getNumber()).isEqualTo(1111111112L);
+        assertThat(result.getAccounts().get(1).getId()).isEqualTo(2L);
+        assertThat(result.getAccounts().get(1).getFullNumber()).isEqualTo(23211111112L);
+        assertThat(result.getAccounts().get(1).getType()).isEqualTo(AccountType.SAVING);
         assertThat(result.getAccounts().get(1).getBalance()).isEqualTo(2000L);
     }
 
-    @DisplayName("계좌를 등록하면 계좌번호를 부여하고 생성한다")
+    @DisplayName("새로운 계좌를 등록한다.")
     @Test
-    public void account_register_success_test1() {
+    void register_new_account() {
         // given
-        Long fullNumber = Long.parseLong(initNormalTypeNumber + "" + 11111111111L);
-        User user = userRepository.save(User.builder().email("test1@tset.com").name("test1").build());
-        AccountRegisterRequestServiceDto accountRegisterRequestServiceDto = AccountRegisterRequestServiceDto.builder().type(AccountType.NORMAL).password("1234").build();
+        User user = User.builder().id(1L).name("test").build();
+        AccountRegisterRequestServiceDto accountRegisterRequestServiceDto = AccountRegisterRequestServiceDto.builder().password("1234").balance(1000L).type(AccountType.NORMAL).build();
+
+        // stub
+        when(accountTypeNumberRepository.findById(any())).thenReturn(Optional.of(AccountTypeNumber.builder().accountType(AccountType.NORMAL).number(232L).build()));
+        when(accountLockService.getNewAccountNumber(any())).thenReturn(11111111111L);
+        when(accountRepository.save(any())).thenReturn(Account.builder().id(1L).fullNumber(23211111111L).number(11111111111L).type(AccountType.NORMAL).balance(1000L).build());
 
         // when
         AccountRegisterResponseDto accountRegisterResponseDto = accountService.register(accountRegisterRequestServiceDto, user);
@@ -98,99 +88,85 @@ class AccountServiceImplTest {
         // then
         assertThat(accountRegisterResponseDto.getId()).isEqualTo(1L);
         assertThat(accountRegisterResponseDto.getNumber()).isEqualTo(11111111111L);
-        assertThat(accountRegisterResponseDto.getFullNumber()).isEqualTo(fullNumber);
-        assertThat(accountRegisterResponseDto.getBalance()).isEqualTo(0L);
+        assertThat(accountRegisterResponseDto.getFullNumber()).isEqualTo(23211111111L);
+        assertThat(accountRegisterResponseDto.getBalance()).isEqualTo(1000L);
     }
 
-    @DisplayName("계좌 종류에 따라 다른 타입 번호가 부여되고 중복된 계좌번호는 부여되지 않는다.")
+    @DisplayName("계좌 가입시 계좌 타입에 해당하는 타입 번호를 찾을 수 없으면 예외를 발생시킨다.")
     @Test
-    public void account_register_success_test2() {
+    void if_not_found_account_type_when_register_account_throw_exception() {
         // given
-        User user1 = userRepository.save(User.builder().email("test1@tset.com").name("test1").build());
-        User user2 = userRepository.save(User.builder().email("test2@tset.com").name("test2").build());
-        AccountRegisterRequestServiceDto accountRegisterRequestServiceDto1 = AccountRegisterRequestServiceDto.builder().type(AccountType.NORMAL).password("1234").build();
-        AccountRegisterRequestServiceDto accountRegisterRequestServiceDto2 = AccountRegisterRequestServiceDto.builder().type(AccountType.SAVING).password("1234").balance(1000L).build();
-        AccountRegisterRequestServiceDto accountRegisterRequestServiceDto3 = AccountRegisterRequestServiceDto.builder().type(AccountType.NORMAL).password("1234").balance(2000L).build();
+        User user = User.builder().id(1L).build();
+        AccountRegisterRequestServiceDto accountRegisterRequestServiceDto = AccountRegisterRequestServiceDto.builder().password("1234").balance(1000L).type(AccountType.NORMAL).build();
 
-        // when
-        AccountRegisterResponseDto accountRegisterResponseDto1 = accountService.register(accountRegisterRequestServiceDto1, user1);
-        AccountRegisterResponseDto accountRegisterResponseDto2 = accountService.register(accountRegisterRequestServiceDto2, user1);
-        AccountRegisterResponseDto accountRegisterResponseDto3 = accountService.register(accountRegisterRequestServiceDto3, user2);
+        // stub
+        when(accountTypeNumberRepository.findById(any())).thenReturn(Optional.empty());
 
-        // then
-        assertThat(accountRegisterResponseDto1.getId()).isEqualTo(1L);
-        assertThat(accountRegisterResponseDto1.getNumber()).isEqualTo(11111111111L);
-        assertThat(accountRegisterResponseDto1.getFullNumber()).isEqualTo(Long.parseLong(initNormalTypeNumber + "" + 11111111111L));
-        assertThat(accountRegisterResponseDto1.getBalance()).isEqualTo(0L);
-        assertThat(accountRegisterResponseDto2.getId()).isEqualTo(2L);
-        assertThat(accountRegisterResponseDto2.getNumber()).isEqualTo(11111111111L);
-        assertThat(accountRegisterResponseDto2.getFullNumber()).isEqualTo(Long.parseLong(initSavingTypeNumber + "" + 11111111111L));
-        assertThat(accountRegisterResponseDto2.getBalance()).isEqualTo(1000L);
-        assertThat(accountRegisterResponseDto3.getId()).isEqualTo(3L);
-        assertThat(accountRegisterResponseDto3.getNumber()).isEqualTo(11111111112L);
-        assertThat(accountRegisterResponseDto3.getFullNumber()).isEqualTo(Long.parseLong(initNormalTypeNumber + "" + 11111111112L));
-        assertThat(accountRegisterResponseDto3.getBalance()).isEqualTo(2000L);
-    }
-
-    @DisplayName("계좌 종류에 해당 하는 타입 넘버가 없으면 예외를 뱉는다.")
-    @Test
-    public void account_register_fail_test1() {
-        // given & when
-        accountTypeNumberRepository.deleteAll();
-        User user = userRepository.save(User.builder().email("test1@tset.com").name("test1").build());
-        AccountRegisterRequestServiceDto accountRegisterRequestServiceDto = AccountRegisterRequestServiceDto.builder().type(AccountType.NORMAL).password("1234").build();
-
-        // then
+        // when & then
         assertThatThrownBy(() -> accountService.register(accountRegisterRequestServiceDto, user))
                 .isInstanceOf(NotFoundAccountTypeNumberException.class);
     }
 
-    @DisplayName("계좌 종류에 해당하는 시퀀스 값이 없으면 예외를 뱉는다.")
+    @DisplayName("자신의 계좌를 삭제할 수 있다.")
     @Test
-    public void account_register_fail_test2() {
-        // given & when
-        accountSequenceRepository.deleteAll();
-        User user = userRepository.save(User.builder().email("test1@tset.com").name("test1").build());
-        AccountRegisterRequestServiceDto accountRegisterRequestServiceDto = AccountRegisterRequestServiceDto.builder().type(AccountType.NORMAL).password("1234").build();
-
-        // then
-        assertThatThrownBy(() -> accountService.register(accountRegisterRequestServiceDto, user))
-                .isInstanceOf(NotFoundAccountSequenceException.class);
-    }
-
-    @DisplayName("자신의 계좌를 없앨 수 있다.")
-    @Test
-    public void account_delete_success_test() {
+    void delete_my_account() {
         // given
-        User user = userRepository.save(User.builder().email("test1@tset.com").name("test1").build());
-        accountRepository.save(Account.builder().number(11111111L).fullNumber(23211111111L).type(AccountType.NORMAL).balance(1000L).user(user).build());
+        Long accountFullNumber = 23211111111L;
+        User user = User.builder().id(1L).email("test1@tset.com").name("test1").build();
+
+        // stub
+        when(accountRepository.findByFullNumber(anyLong())).thenReturn(Optional.of(Account.builder().id(1L).fullNumber(23211111111L).type(AccountType.NORMAL).balance(1000L).user(user).build()));
 
         // when
-        accountService.deleteAccount(23211111111L, user);
+        accountService.deleteAccount(accountFullNumber, user);
 
         // then
-        Optional<Account> account = accountRepository.findByFullNumber(23211111111L);
-        assertThat(account.isPresent()).isFalse();
+        verify(accountRepository, times(1)).deleteById(anyLong());
     }
 
-    @DisplayName("계좌번호가 일치하지 않으면 삭제할 수 없다.")
+    @DisplayName("계좌 삭제시 계좌번호를 찾을 수 없으면 예외를 발생시킨다.")
     @Test
-    public void account_delete_fail_test1() {
-        // given & when
-        User user = userRepository.save(User.builder().email("test1@tset.com").name("test1").build());
-        accountRepository.save(Account.builder().number(11111111L).fullNumber(23211111111L).type(AccountType.NORMAL).balance(1000L).user(user).build());
+    public void if_not_found_account_number_when_delete_account_throw_exception() {
+        // given
+        User user = User.builder().email("test1@tset.com").name("test1").build();
         Long notExistAccount = 23299999999L;
 
-        // then
+        // stub
+        when(accountRepository.findByFullNumber(anyLong())).thenReturn(Optional.empty());
+
+        // when & then
         assertThatThrownBy(() -> accountService.deleteAccount(notExistAccount, user))
                 .isInstanceOf(NotFoundAccountFullNumberException.class);
     }
 
+    @DisplayName("계좌 삭제시 자신의 계좌가 아니라면 예외를 발생시킨다.")
     @Test
-    void account_deposit_success_test() {
+    public void if_not_account_owner_when_delete_account_throw_exception() {
         // given
-        accountRepository.save(Account.builder().number(11111111L).fullNumber(23211111111L).type(AccountType.NORMAL).balance(1000L).build());
+        Long accountNumber = 23211111111L;
+        User user = User.builder().id(1L).email("test1@tset.com").name("test1").build();
+        User anotherUser = User.builder().id(2L).email("test2test.com").name("test2").build();
+        Account account = Account.builder().id(1L).fullNumber(23211111111L).type(AccountType.NORMAL).balance(1000L).user(user).build();
+
+        // stub
+        when(accountRepository.findByFullNumber(anyLong())).thenReturn(Optional.of(account));
+
+        // when & then
+        assertThatThrownBy(() -> accountService.deleteAccount(accountNumber, anotherUser))
+                .isInstanceOf(NotAccountOwnerException.class);
+    }
+
+    @DisplayName("계좌에 돈을 입금한다.")
+    @Test
+    void deposit_account() {
+        // given
         AccountDepositRequestServiceDto accountDepositRequestServiceDto = AccountDepositRequestServiceDto.builder().fullNumber(23211111111L).amount(1000L).transactionType(TransactionType.DEPOSIT).sender("ATM").tel("010-1234-1234").build();
+        Account account = Account.builder().id(1L).fullNumber(23211111111L).type(AccountType.NORMAL).balance(1000L).build();
+        Transaction transaction = Transaction.builder().id(1L).type(TransactionType.DEPOSIT).sender("ATM").amount(1000L).depositAccountBalance(2000L).tel("010-1234-1234").createdAt(LocalDateTime.of(2023, 8, 11, 15, 30, 00)).build();
+
+        // stub
+        when(accountLockService.depositAccountWithLock(anyLong(), anyLong())).thenReturn(account);
+        when(transactionRepository.save(any())).thenReturn(transaction);
 
         // when
         AccountDepositResponseDto result = accountService.deposit(accountDepositRequestServiceDto);
@@ -206,6 +182,33 @@ class AccountServiceImplTest {
         assertThat(result.getTransaction().getTel()).isEqualTo("010-1234-1234");
         assertThat(result.getTransaction().getCreatedAt()).isEqualTo("2023-08-11T15:30");
     }
-    
-    // 출금 테스트 만들어야함
+
+    @DisplayName("계좌에서 돈을 출금한다.")
+    @Test
+    void withdraw_account() {
+        // given
+        AccountWithdrawRequestServiceDto accountWithdrawRequestServiceDto = AccountWithdrawRequestServiceDto.builder().fullNumber(23211111111L).amount(1000L).transactionType(TransactionType.WITHDRAW).build();
+        Account account = Account.builder().id(1L).fullNumber(23211111111L).type(AccountType.NORMAL).balance(1000L).build();
+        User user = User.builder().id(1L).build();
+        Transaction transaction = Transaction.builder().id(1L).type(TransactionType.WITHDRAW).sender("ATM").receiver("23211111111").amount(1000L).withdrawAccountBalance(2000L).createdAt(LocalDateTime.of(2023, 8, 11, 15, 30, 00)).build();
+
+        // stub
+        when(accountLockService.withdrawWithLock(any())).thenReturn(account);
+        when(transactionRepository.save(any())).thenReturn(transaction);
+
+        // when
+        AccountWithdrawResponseDto result = accountService.withdraw(accountWithdrawRequestServiceDto, user);
+
+        // then
+        assertThat(result.getId()).isEqualTo(1L);
+        assertThat(result.getFullNumber()).isEqualTo(23211111111L);
+        assertThat(result.getBalance()).isEqualTo(1000L);
+        assertThat(result.getTransaction().getId()).isEqualTo(1L);
+        assertThat(result.getTransaction().getType()).isEqualTo(TransactionType.WITHDRAW.name());
+        assertThat(result.getTransaction().getSender()).isEqualTo("ATM");
+        assertThat(result.getTransaction().getReceiver()).isEqualTo("23211111111");
+        assertThat(result.getTransaction().getAmount()).isEqualTo(1000L);
+        assertThat(result.getTransaction().getWithdrawAccountBalance()).isEqualTo(2000L);
+        assertThat(result.getTransaction().getCreatedAt()).isEqualTo("2023-08-11T15:30");
+    }
 }
