f47e2d5653ae79e5d06777e6029889efbb1019d4
Woosung_Kim <sibu2005@naver.com>
2023-10-18 22:48:20 +0900

    feat : 계좌이체 동시성 테스트 추가 및 수정 - POJO 객체인걸 까먹고 삽질하다 시간 매우 많이 날림 - 주의!!

--- src/test/java/shop/woosung/bank/account/service/AccountConcurrencyServiceTest.java
+++ src/test/java/shop/woosung/bank/account/service/AccountConcurrencyServiceTest.java
@@ -31,7 +31,6 @@ import static org.mockito.Mockito.when;
 @ActiveProfiles("test")
 @SpringBootTest
 class AccountConcurrencyServiceTest {
-
     @Autowired
     private AccountService accountService;
     @Autowired
@@ -50,18 +49,20 @@ class AccountConcurrencyServiceTest {
     @DisplayName("계좌 생성 요청을 동시에 100개를 보냈을 때 계좌번호가 중복되는 계좌가 없어야 한다.")
     @Test
     void account_register_100_concurrent_requests_not_have_duplicate_accounts() throws Exception {
-        int threadCount = 100;
+        // given
         User user = userRepository.save(User.builder().email("testcon@test.com").password("1234").name("test").role(UserRole.CUSTOMER).build());
-        ExecutorService executorService = Executors.newFixedThreadPool(25);
-
-        List<Long> accountFullNumbers = Collections.synchronizedList(new ArrayList<>());
 
         AccountRegisterRequestServiceDto normalAccountRegisterServiceDto = AccountRegisterRequestServiceDto.builder()
                 .type(AccountType.NORMAL)
                 .password("1111")
                 .build();
 
+        List<Long> accountFullNumbers = Collections.synchronizedList(new ArrayList<>());
+        ExecutorService executorService = Executors.newFixedThreadPool(25);
+        int threadCount = 100;
         CountDownLatch latch = new CountDownLatch(threadCount);
+
+        // when
         for (int i = 0; i < threadCount; i++) {
             executorService.submit(() -> {
                 try {
@@ -77,6 +78,7 @@ class AccountConcurrencyServiceTest {
 
         Set<Long> accountFullNemberSet = new HashSet<>(accountFullNumbers);
 
+        // then
         assertThat(accountFullNumbers).hasSize(threadCount);
         assertThat(accountFullNemberSet).hasSize(threadCount);
     }
@@ -84,12 +86,9 @@ class AccountConcurrencyServiceTest {
     @DisplayName("계좌 입금 요청을 동시에 100개를 보냈을 때 최종 금액이 모든 요청 금액을 합친 것과 같아야한다.")
     @Test
     void account_deposit_100_concurrent_requests_not_() throws Exception {
-        int threadCount = 100;
+        // given
         User user = userRepository.save(User.builder().email("testcon2@test.com").password("1234").name("test").role(UserRole.CUSTOMER).build());
         accountRepository.save(Account.builder().number(11111111L).fullNumber(23411111111L).password("aaaa-bbbb-cccc").balance(1000L).type(AccountType.NORMAL).user(user).build());
-        AtomicLong maxAmount = new AtomicLong(0);
-
-        ExecutorService executorService = Executors.newFixedThreadPool(25);
 
         AccountDepositRequestServiceDto accountDepositRequestServiceDto = AccountDepositRequestServiceDto.builder()
                 .fullNumber(23411111111L)
@@ -99,7 +98,12 @@ class AccountConcurrencyServiceTest {
                 .tel("010-1111-1111")
                 .build();
 
+        AtomicLong maxAmount = new AtomicLong(0);
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(25);
         CountDownLatch latch = new CountDownLatch(threadCount);
+
+        // when
         for (int i = 0; i < threadCount; i++) {
             executorService.submit(() -> {
                 try {
@@ -114,18 +118,16 @@ class AccountConcurrencyServiceTest {
         latch.await();
         executorService.shutdown();
 
+        // then
         assertThat(maxAmount.get()).isEqualTo(101000L);
     }
 
     @DisplayName("계좌 출금 요청을 동시에 100개를 보냈을 때 최종 금액이 모든 요청 금액을 뺀 것과 같아야한다.")
     @Test
     void account_withdraw_100_concurrent_requests_not_() throws Exception {
-        int threadCount = 100;
+        // given
         User user = userRepository.save(User.builder().email("testcon3@test.com").password("aaaa-bbbb-cccc").name("test").role(UserRole.CUSTOMER).build());
         accountRepository.save(Account.builder().number(11111111L).fullNumber(23311111112L).password("aaaa-bbbb-cccc").balance(999999L).type(AccountType.NORMAL).user(user).build());
-        AtomicLong maxAmount = new AtomicLong(999999L);
-
-        ExecutorService executorService = Executors.newFixedThreadPool(25);
 
         AccountWithdrawRequestServiceDto accountWithdrawRequestServiceDto = AccountWithdrawRequestServiceDto.builder()
                 .fullNumber(23311111112L)
@@ -134,15 +136,18 @@ class AccountConcurrencyServiceTest {
                 .password("aaaa-bbbb-cccc")
                 .build();
 
+        AtomicLong maxAmount = new AtomicLong(999999L);
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(25);
         CountDownLatch latch = new CountDownLatch(threadCount);
+
+        // when
         for (int i = 0; i < threadCount; i++) {
             executorService.submit(() -> {
                 try {
                     AccountWithdrawResponseDto accountWithdrawResponseDto = accountService.withdraw(accountWithdrawRequestServiceDto, user);
                     long currentBalance = accountWithdrawResponseDto.getTransaction().getWithdrawAccountBalance();
                     maxAmount.accumulateAndGet(currentBalance, Math::min);
-                } catch(Exception e) {
-                    System.out.println("e = " + e);
                 } finally {
                     latch.countDown();
                 }
@@ -151,6 +156,52 @@ class AccountConcurrencyServiceTest {
         latch.await();
         executorService.shutdown();
 
+        // then
         assertThat(maxAmount.get()).isEqualTo(989999L);
+    }
+
+    @DisplayName("계좌 이체 요청을 동시에 100개를 보냈을 때 동시성 문제 없이 모두 정상적으로 이체된다.")
+    @Test
+    void account_transfer_100_concurrent_success() throws Exception {
+        // given
+        User user = userRepository.save(User.builder().email("testcon3@test.com").password("aaaa-bbbb-cccc").name("test").role(UserRole.CUSTOMER).build());
+        accountRepository.save(Account.builder().number(11111111L).fullNumber(23711111111L).password("aaaa-bbbb-cccc").balance(100000L).type(AccountType.NORMAL).user(user).build());
+        accountRepository.save(Account.builder().number(11111112L).fullNumber(23711111112L).password("aaaa-bbbb-cccc").balance(0L).type(AccountType.NORMAL).user(user).build());
+
+        AccountTransferRequestServiceDto accountTransferRequestServiceDto = AccountTransferRequestServiceDto.builder()
+                .withdrawFullNumber(23711111111L)
+                .depositFullNumber(23711111112L)
+                .withdrawPassword(1234L)
+                .amount(1000L)
+                .transactionType(TransactionType.TRANSFER)
+                .build();
+
+        int threadCount = 100;
+        
+        AtomicLong withdrawAccountBalance = new AtomicLong(999999L);
+        AtomicLong depositAccountBalance = new AtomicLong(0L);
+        ExecutorService executorService = Executors.newFixedThreadPool(25);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    AccountTransferResponseDto accountTransferResponseDto = accountService.transfer(accountTransferRequestServiceDto, user);
+                    long withdrawBalance = accountTransferResponseDto.getBalance();
+                    long depositBalance = accountTransferResponseDto.getTransaction().getDepositAccountBalance();
+                    withdrawAccountBalance.accumulateAndGet(withdrawBalance, Math::min);
+                    depositAccountBalance.accumulateAndGet(depositBalance, Math::max);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+        executorService.shutdown();
+
+        // then
+        assertThat(withdrawAccountBalance.get()).isEqualTo(0L);
+        assertThat(depositAccountBalance.get()).isEqualTo(100000L);
     }
 }
