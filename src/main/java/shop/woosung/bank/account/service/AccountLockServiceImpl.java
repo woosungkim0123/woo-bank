7999f9139cc79d25ac7cf991936cb26ac0cc8728
Woosung_Kim <sibu2005@naver.com>
2023-10-18 23:15:20 +0900

    feat : 데드락 순환 대기 방지 추가

--- src/main/java/shop/woosung/bank/account/service/AccountLockServiceImpl.java
+++ src/main/java/shop/woosung/bank/account/service/AccountLockServiceImpl.java
@@ -10,6 +10,7 @@ import shop.woosung.bank.account.domain.AccountSequence;
 import shop.woosung.bank.account.domain.AccountType;
 import shop.woosung.bank.account.handler.exception.NotFoundAccountFullNumberException;
 import shop.woosung.bank.account.handler.exception.NotFoundAccountSequenceException;
+import shop.woosung.bank.account.handler.exception.SameAccountTransferException;
 import shop.woosung.bank.account.service.dto.AccountTransferLockResponseDto;
 import shop.woosung.bank.account.service.dto.AccountTransferLockServiceDto;
 import shop.woosung.bank.account.service.dto.AccountWithdrawLockServiceDto;
@@ -51,11 +52,8 @@ public class AccountLockServiceImpl implements AccountLockService {
         Account withdrawAccount = getAccountWithLock(accountWithdrawLockServiceDto.getFullNumber());
 
         withdrawAccount.checkOwner(accountWithdrawLockServiceDto.getUser().getId());
-
         withdrawAccount.checkPasswordMatch(accountWithdrawLockServiceDto.getPassword(), passwordEncoder);
-        
         withdrawAccount.checkEnoughBalance(accountWithdrawLockServiceDto.getAmount());
-
         withdrawAccount.withdraw(accountWithdrawLockServiceDto.getAmount());
 
         accountRepository.update(withdrawAccount);
@@ -66,16 +64,39 @@ public class AccountLockServiceImpl implements AccountLockService {
     @Override
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public AccountTransferLockResponseDto transferWithLock(AccountTransferLockServiceDto accountTransferLockServiceDto) {
-        Account withdrawAccount = getAccountWithLock(accountTransferLockServiceDto.getWithdrawFullNumber());
-        Account depositAccount = getAccountWithLock(accountTransferLockServiceDto.getDepositFullNumber());
+        checkSameAccount(accountTransferLockServiceDto.getWithdrawFullNumber(), accountTransferLockServiceDto.getDepositFullNumber());
+
+        /**
+         * 데드락 방지 전략 - 순환 대기 방지
+         * 항상 계좌 번호가 작은 순서대로 락을 획득하여 아래와 같은 데드락 상황을 방지한다.
+         *
+         * 데드락이 발생할 수 있는 예시:
+         * 1. 스레드 A가 1111 계좌의 락을 획득.
+         * 2. 스레드 B가 1112 계좌의 락을 획득.
+         * 3. 스레드 A는 1112 계좌의 락을 대기.
+         * 4. 스레드 B는 1111 계좌의 락을 대기.
+         * 결과: 데드락 발생!
+         */
+        Account withdrawAccount;
+        Account depositAccount;
+
+        if(accountTransferLockServiceDto.getWithdrawFullNumber() < accountTransferLockServiceDto.getDepositFullNumber()) {
+            withdrawAccount = getAccountWithLock(accountTransferLockServiceDto.getWithdrawFullNumber());
+            depositAccount = getAccountWithLock(accountTransferLockServiceDto.getDepositFullNumber());
+        } else {
+            depositAccount = getAccountWithLock(accountTransferLockServiceDto.getDepositFullNumber());
+            withdrawAccount = getAccountWithLock(accountTransferLockServiceDto.getWithdrawFullNumber());
+        }
 
         withdrawAccount.checkOwner(accountTransferLockServiceDto.getUser().getId());
         withdrawAccount.checkPasswordMatch(String.valueOf(accountTransferLockServiceDto.getWithdrawPassword()), passwordEncoder);
-
         withdrawAccount.checkEnoughBalance(accountTransferLockServiceDto.getAmount());
         withdrawAccount.withdraw(accountTransferLockServiceDto.getAmount());
 
         depositAccount.deposit(accountTransferLockServiceDto.getAmount());
+
+        accountRepository.update(withdrawAccount);
+        accountRepository.update(depositAccount);
 
         return AccountTransferLockResponseDto.from(withdrawAccount, depositAccount);
     }
@@ -83,5 +104,11 @@ public class AccountLockServiceImpl implements AccountLockService {
     private Account getAccountWithLock(Long fullNumber) {
         return accountRepository.findByFullNumberWithPessimisticLock(fullNumber)
                 .orElseThrow(() -> new NotFoundAccountFullNumberException(fullNumber));
+    }
+
+    private void checkSameAccount(Long withdrawFullNumber, Long depositFullNumber) {
+        if(withdrawFullNumber.equals(depositFullNumber)) {
+            throw new SameAccountTransferException(withdrawFullNumber);
+        }
     }
 }
